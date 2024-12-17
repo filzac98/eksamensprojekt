18811a6333d0c2f44dfeb075f52ad094f7c75740
Filzac <fiza0001@stud.kea.dk>
2024-12-17 23:35:14 +0000

    burger menu fix

--- src/script/burgerMenu.js
+++ src/script/burgerMenu.js
@@ -5,11 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const navMenu = document.querySelector(".nav-menu");
   const menuText = document.querySelector(".menu-text");
+  const cartIcon = document.querySelector(".cart-icon");
 
   // Toggle nav menu and update burger color & menu text color
   burgerMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
     burgerMenu.classList.toggle("active");
     menuText.classList.toggle("active");
+    cartIcon.style.display = navMenu.classList.contains("active")
+      ? "none"
+      : "block";
   });
 });
